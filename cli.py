f1424478ddebb7091ecc18e200259a91f5501df2
Linus <element.oxygen@gmail.com>
2023-02-17 22:43:23 +0100

    added similar lookup

--- cli.py
+++ cli.py
@@ -11,7 +11,8 @@ def load_db():
         db = json.load(openfile)
     return db
 
-def ingredient_lookup(search):
+
+def ingredient_search(search):
     ingredients = load_db()
     keys = [key for key in ingredients]
     result = process.extractOne(search, keys)
@@ -30,6 +31,7 @@ def ingredient_lookup(search):
             for result, confidence in similar:
                 print(f"{result}")
 
+
 def similar_finder():
     db = load_db()
     for key in db:
@@ -37,18 +39,31 @@ def similar_finder():
         for result in results:
             if result[0] != key:
                 if result[1] > 90:
-                    #print(f"{key} is similar to {result[0]} - Similarity Grade {result[1]}")
+                    # print(f"{key} is similar to {result[0]} - Similarity Grade {result[1]}")
                     print(f'"{result[0]}": "{key}",')
+
+
+def ingredient_lookup(lookup):
+    db = load_db()
+    results = process.extract(lookup, db.keys())
+    for result in results:
+        if result[0].lower() != lookup.lower():
+            if result[1] > 50:
+                print(f"Similar to {result[0]} - Similarity Grade {result[1]}")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", required=False)
     parser.add_argument("-s", action='store_true', required=False)
+    parser.add_argument("-l", required=False)
     args = parser.parse_args()
     pprint(args)
     ingredient = args.i
+    lookup = args.l
     if ingredient:
-        ingredient_lookup(ingredient)
-    if args.s:
+        ingredient_search(ingredient)
+    if lookup:
+        ingredient_lookup(lookup)
+    elif args.s:
         similar_finder()
